7b1dc954f1cd2d11e86295c086d2d1638eb4451c
GuilhermeLouli <guilhermeloulisoares@gmail.com>
2017-09-01 23:06:07 -0300

    Correçao nomenclatura 4

--- Solutions/Solution_Problem_4.py
+++ Solutions/Solution_Problem_4.py
@@ -1,7 +1,7 @@
-x = int(input("Qual seu peso?    "))
-y = int(input("Qual seu tamanho?    "))
-def data(mass, height):
+mass = int(input("Qual seu peso?    "))
+height = int(input("Qual seu tamanho?    "))
+def calcIMC(mass, height):
     mass = mass * 10000
-    phisicalCondition = mass / height ** 2 
-    print(phisicalCondition)
-data(x,y)
+    phisicalCondition = mass / (height ** 2) 
+    print("IMC: ", phisicalCondition)
+calcIMC(mass, height)
