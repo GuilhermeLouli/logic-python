ab55cdf13a32de8fa606c92af3af965cc5898a3d
GuilhermeLouli <guilhermeloulisoares@gmail.com>
2017-09-01 23:06:20 -0300

    Correçao nomenclatura 5

--- Solutions/Solution_Problem_5.py
+++ Solutions/Solution_Problem_5.py
@@ -1,8 +1,8 @@
-bimestre1 = int(input("bimestre 1    "))
-bimestre2 = int(input("bimestre 2    "))
-bimestre3 = int(input("bimestre 3    "))
-bimestre4 = int(input("bimestre 4    "))
-def calcAverage(a,b,c,d):
-    average = (a + b + c + d) / 4
+bimestre1 = int(input("bimestre 1:    "))
+bimestre2 = int(input("bimestre 2:    "))
+bimestre3 = int(input("bimestre 3:    "))
+bimestre4 = int(input("bimestre 4:    "))
+def calcAverage(bimestre1, bimestre2, bimestre3, bimestre4):
+    average = (bimestre1 + bimestre2 + bimestre3 + bimestre4) / 4
     print(average)
-bimestres(w,x,y,z)
+calcAverage(bimestre1, bimestre2, bimestre3, bimestre4)
