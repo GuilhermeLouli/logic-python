b66b20fde9732d73f1a5725c171659e889559c36
GuilhermeLouli <guilhermeloulisoares@gmail.com>
2017-09-01 21:39:11 -0300

    Problem5

---
+++ Solutions/Solution_Problem_5.py
@@ -0,0 +1,8 @@
+w = int(input("bimestre 1    "))
+x = int(input("bimestre 2    "))
+y = int(input("bimestre 3    "))
+z = int(input("bimestre 4    "))
+def bimestres(a,b,c,d):
+    average = (a + b + c + d) / 4
+    print(average)
+bimestres(w,x,y,z)
